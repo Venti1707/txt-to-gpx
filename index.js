5229fda2c25cd5dbc3eab7eef09fffdb9f48dc58
NicKohSE3 <nickohse3@gmail.com>
2022-10-10 17:58:18 +0800

    Update index.js
    - Added <trk> & <trkpt> for distance calculation using https://www.sportdistancecalculator.com/import-gpx.php
    - Added comments for final GPX file to make it easier to spot when certain tags start and end

--- index.js
+++ index.js
@@ -7,8 +7,59 @@ function uniqueResults(value, index, self) {
 
 const generateGPX = (sortedArray) =>
   `<?xml version="1.0" encoding="utf-8"?>
+<!-- GPX begins -->
 <gpx version="1.1">
+\t<!-- Route begins -->
 \t<rte>
+\t\t<!-- Route point begins -->
+${sortedArray
+    .map((p) => {
+      var [lat, lon] = p.split(",");
+      lat = lat.trim();
+      lon = lon.trim();
+
+      // Check for errors within latitude values
+      if (lat.length === 0) {
+        console.error(C.redBright("There is a missing latitude value somewhere, please check your input file again."));
+        process.exit();
+      }
+
+      if (isNaN(lat)) {
+        console.error(C.redBright("One of the latitude values is not a number, please check your input file again."));
+        process.exit();
+      }
+
+      if (lat > 90 || lat < -90) {
+        console.error(C.redBright("One of the latitude values falls outside the acceptable range, please check your input file again."));
+        process.exit();
+      }
+
+      // Check for errors within longitude values
+      if (lon.length === 0) {
+        console.error(C.redBright("There is a missing longitude value somewhere, please check your input file again."));
+        process.exit();
+      }
+
+      if (isNaN(lon)) {
+        console.error(C.redBright("One of the longitude values is not a number, please check your input file again."));
+        process.exit();
+      }
+
+      if (lon > 180 || lat < -180) {
+        console.error(C.redBright("One of the longitude values falls outside the acceptable range, please check your input file again."));
+        process.exit();
+      }
+      return `\t\t<rtept lat="${+lat}" lon="${+lon}"></rtept>`;
+    })
+    .join(`\n`)
+  }
+\t\t<!-- Route point ends -->
+\t</rte>
+\t<!-- Route ends -->
+
+\t<!-- Track begins -->
+\t<trk>
+\t\t<!-- Track point begins -->
 ${sortedArray
     .map((p) => {
       var [lat, lon] = p.split(",");
@@ -47,13 +98,16 @@ ${sortedArray
         process.exit();
       }
 
-      return `\t\t<rtept lat="${+lat}" lon="${+lon}"></rtept>`;
+      return `\t\t<trkpt lat="${+lat}" lon="${+lon}"></trkpt>`;
 
     })
     .join(`\n`)
   }
-\t</rte>
+\t\t<!-- Track point ends -->
+\t</trk>
+\t<!-- Track ends -->
 
+\t<!-- Waypoints and name begins -->
 ${sortedArray
     .map((p) => {
       var [lat, lon, loc] = p.split(",");
@@ -102,7 +156,9 @@ ${sortedArray
     })
     .join(`\n`)
   }
-</gpx>`;
+\t<!-- Waypoints and name ends -->
+</gpx>
+<!-- GPX ends -->`;
 
 
 const GPX = (contents) => {
